1b3f6697ff15874e9c78bbab7802400f1f4c888e
Martins Ojo <martinsojoola2@gmail.com>
2022-11-10 00:55:23 +0100

    fix comment style

--- open_shell.c
+++ open_shell.c
@@ -8,12 +8,12 @@
  */
 int main(int argc, char **argv)
 {
-	/* Load config files, if any.
+	/* Load config files, if any. */
 
-	/* Run command loop.
+	/* Run command loop. */
 	lsh_loop();
 
-	/* Perform any shutdown/cleanup.
+	/* Perform any shutdown/cleanup. */
 
 	return EXIT_SUCCESS;
 }
